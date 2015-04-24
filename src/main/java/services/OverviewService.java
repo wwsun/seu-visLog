2fb287da795cb1fa5f5c571ebc5c2a6668794d65
Weiwei SUN <ww.sun@outlook.com>
2015-04-24 17:57:57 +0800

    update getSessionCountsAndBounceRate

--- src/main/java/services/OverviewService.java
+++ src/main/java/services/OverviewService.java
@@ -4,18 +4,22 @@ import com.mongodb.DB;
 import com.mongodb.DBObject;
 import dao.JumpDAO;
 import dao.SessionDAO;
+import dao.SourceDAO;
 
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 
 public class OverviewService {
 
     SessionDAO sessionDAO;
     JumpDAO jumpDAO;
+    SourceDAO sourceDAO;
 
     public OverviewService(final DB db) {
         sessionDAO = new SessionDAO(db);
         jumpDAO = new JumpDAO(db);
+        sourceDAO = new SourceDAO(db);
     }
 
     public DBObject getSessionDistributionByDate(String date) {
@@ -24,14 +28,18 @@ public class OverviewService {
 
     /**
      *
-     * @return {"total":"COUNT", "onehop":"PERCENT"}
+     * @return {"total":"COUNT", "bounceRate":"PERCENT", "inquiryRate":"PERCENT"}
      */
     public JsonObject getSessionCountsAndBounceRate() {
         return Json.createObjectBuilder()
                 .add("total", jumpDAO.getSessionCounts())
-                .add("onehop", jumpDAO.getBounceRate())
+                .add("bounce_rate", jumpDAO.getBounceRate())
+                .add("inquiry_rate", jumpDAO.getInquiryRate())
                 .build();
+    }
 
+    public JsonArray getTopSearchEngines(int limit) {
+        return sourceDAO.getTopSearchEngines(limit);
     }
 
 }
