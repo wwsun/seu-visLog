f0d111f1b6a858da449f614d9aa865b10febd83e
Weiwei SUN <ww.sun@outlook.com>
2015-04-24 14:10:10 +0800

    application root controller

---
+++ src/main/java/controllers/Application.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * Created by Weiwei on 2015/4/22.
+ */
+
+@ApplicationPath("/api")
+public class Application extends javax.ws.rs.core.Application {
+
+}
