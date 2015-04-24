5e20ee06d31cb6e075549e6c9eb00afcabb6e558
Weiwei SUN <ww.sun@outlook.com>
2015-04-24 14:09:38 +0800

    junit test case

---
+++ src/test/java/SessionTest.java
@@ -0,0 +1,33 @@
+import com.mongodb.DB;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import dao.SessionDAO;
+import junit.framework.TestCase;
+import services.OverviewService;
+
+import java.net.UnknownHostException;
+
+public class SessionTest extends TestCase {
+
+
+    final String mongoURI = "mongodb://223.3.80.243:27017";
+    final MongoClient mongoClient;
+    final DB siteDatabase;
+
+    public SessionTest() throws UnknownHostException {
+        mongoClient = new MongoClient(new MongoClientURI(mongoURI));
+        siteDatabase = mongoClient.getDB("sample");
+    }
+
+    public void testSessionTrends() {
+        SessionDAO sessionDAO = new SessionDAO(siteDatabase);
+        DBObject result = sessionDAO.getSessionsByDate("2014-10-22");
+        System.out.println(result.toString());
+    }
+
+    public void testSessionCounts() {
+        OverviewService overviewService = new OverviewService(siteDatabase);
+        System.out.println(overviewService.getSessionCountsAndBounceRate());
+    }
+}
