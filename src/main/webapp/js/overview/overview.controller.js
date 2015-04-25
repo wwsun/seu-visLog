62000da5fc45e6de8cc2bc574a1f8541ed2432e0
Weiwei SUN <ww.sun@outlook.com>
2015-04-25 22:14:30 +0800

    consuming REST api of overview service

--- src/main/webapp/js/overview/overview.controller.js
+++ src/main/webapp/js/overview/overview.controller.js
@@ -1,6 +1,41 @@
 angular.module('vislog.overview', ['chart.js'])
 
-    .controller('OverviewCtrl', function () {
+    .constant('baseUrl', 'http://localhost:8080/vislog-restful/api/')
+    .controller('OverviewCtrl', function ($http, baseUrl) {
 
         var vm = this;
+
+        vm.status = null;
+        vm.distribution = {
+            labels: [],
+            data: [],
+            series:[]
+        };
+
+        vm.searchEngines = null;
+
+        vm.getOverviewNumbers = function() {
+            $http.get(baseUrl + 'sessions/overview/status').success(function (data) {
+                vm.status = data;
+            });
+        };
+
+        vm.getSessionDistributionByDate = function (date) {
+            $http.get(baseUrl + 'sessions/distribution/' + date).success(function (data) {
+                vm.distribution.labels = data.hour;
+                vm.distribution.data.push(data.dup);
+                vm.distribution.series.push('sessions');
+            });
+        };
+
+        vm.getSearchEngineContribution = function () {
+            $http.get(baseUrl + 'sessions/overview/sources/se').success(function (data) {
+                vm.searchEngines = data;
+            });
+        };
+
+        vm.getOverviewNumbers();
+        vm.getSessionDistributionByDate("2014-10-22");
+        vm.getSearchEngineContribution();
+
     });
