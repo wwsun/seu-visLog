546260f593364cb6cf58142019a0193e200ee419
Weiwei SUN <ww.sun@outlook.com>
2015-04-26 16:17:59 +0800

    country flow contribution

--- src/main/java/services/OverviewService.java
+++ src/main/java/services/OverviewService.java
@@ -2,24 +2,30 @@ package services;
 
 import com.mongodb.DB;
 import com.mongodb.DBObject;
+import com.sun.javafx.scene.layout.region.BackgroundImage;
+import dao.CountryDAO;
 import dao.JumpDAO;
 import dao.SessionDAO;
 import dao.SourceDAO;
 
 import javax.json.Json;
 import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
+import java.util.List;
 
 public class OverviewService {
 
     SessionDAO sessionDAO;
     JumpDAO jumpDAO;
     SourceDAO sourceDAO;
+    CountryDAO countryDAO;
 
     public OverviewService(final DB db) {
         sessionDAO = new SessionDAO(db);
         jumpDAO = new JumpDAO(db);
         sourceDAO = new SourceDAO(db);
+        countryDAO = new CountryDAO(db);
     }
 
     public DBObject getSessionDistributionByDate(String date) {
@@ -47,7 +53,6 @@ public class OverviewService {
         return sourceDAO.getTopSearchEngines(limit);
     }
 
-
     /**
      *
      * @param limit is the most number of results that returned
@@ -68,6 +73,22 @@ public class OverviewService {
 
         // todo:
         return null;
+    }
+
+    /**
+     *
+     * @param limit is the result you want to returned
+     * @return [{name:"XXX", dup:"XXX"}]
+     */
+    public JsonArray getTopCountriesFlow(int limit) {
+        JsonArrayBuilder builder = Json.createArrayBuilder();
+        List<DBObject> list = countryDAO.getGeoDistribution(limit);
+        for (DBObject object : list) {
+            builder.add(Json.createObjectBuilder()
+                            .add("name",(String)object.get("name"))
+                            .add("dup", (int)object.get("value")));
+        }
+        return builder.build();
     }
 
 }
