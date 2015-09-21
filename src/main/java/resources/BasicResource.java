76a2044b0dff00f8e8e7d43a45f64e9b7d31d3d6
Fei Tian <tengfeimoon@163.com>
2015-09-21 20:27:01 +0800

    remove svmlib directory. Add dependencies into pom.xml. MongoDB properties file is modified in order to deploy the app into tomcat without IDE

--- src/main/java/resources/BasicResource.java
+++ src/main/java/resources/BasicResource.java
@@ -1,6 +1,7 @@
 package resources;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
@@ -9,13 +10,18 @@ import java.util.Properties;
  * Created by Administrator on 2015/3/30.
  */
 public class BasicResource {
-    private String fileName="../webapps/vislog/DBConf.properties";
+    private String fileName=".."+ File.separator +"webapps" + File.separator +
+            "vislog-1.0" + File.separator +"DBConf.properties";
+    //private String fileName="DBConf.properties";
     protected String uri;
     protected String db;
 
     public BasicResource(String URI,String DB){
+
         Properties p = new Properties();
         try {
+
+
             InputStream in = new BufferedInputStream(new FileInputStream(fileName));
             p.load(in);
         }
