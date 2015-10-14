95f6cac580efe6232140d12043f79952d14738bf
cmr95 <cmr95@users.noreply.github.com>
2015-10-14 15:49:12 +0800

    Update overview.controller.js
    Add the functions by date range

--- src/main/webapp/js/overview/overview.controller.js
+++ src/main/webapp/js/overview/overview.controller.js
@@ -7,7 +7,8 @@ angular.module('vislog.overview', ['chart.js'])
 
         vm.status = null;
 
-        vm.date = "2014-10-22";
+        vm.startDate = "2014-08-08";
+        vm.endDate = "2014-08-12";
 
         vm.distribution = {
             labels: [],
@@ -39,22 +40,43 @@ angular.module('vislog.overview', ['chart.js'])
             });
         };
 
-        vm.getSessionDistributionByDate = function (date) {
-            $http.get(baseUrl + 'sessions/distribution/' + date).success(function (data) {
+        vm.getOverviewNumbersByRange = function(startDate,endDate) {
+            $http.get(baseUrl + 'sessions/overview/status/' + startDate + '/' + endDate).success(function (data) {
+                vm.status = data;
+            });
+        };
+
+        vm.getSessionDistributionByRange = function (startDate,endDate) {
+            $http.get(baseUrl + 'sessions/distribution/' + startDate + '/' + endDate).success(function (data) {
+                vm.distribution.data = [];
+                vm.distribution.series = [];
                 vm.distribution.labels = data.hour;
                 vm.distribution.data.push(data.dup);
                 vm.distribution.series.push('sessions');
             });
         };
 
-        vm.handleNewAnalysisRangeBtn = function (newDate) {
-            if (angular.isDefined(newDate)) {
-                vm.getSessionDistributionByDate(newDate);
+        vm.handleNewAnalysisRangeBtn = function (startDate,endDate) {
+            if (angular.isDefined(startDate) && angular.isDefined(endDate)) {
+                vm.getSessionDistributionByRange(startDate,endDate);
+                vm.getOverviewNumbersByRange(startDate,endDate);
+                vm.getSearchEngineContributionByRange(startDate,endDate);
+                vm.getCountriesContributionByRange(startDate,endDate);
+                vm.getHotPagesByRange(startDate,endDate);
+                vm.getHotCategoriesByRange(startDate,endDate);
+                vm.getMainLandingCategoriesByRange(startDate,endDate);
+                vm.getMainDropOffCategoriesByRange(startDate,endDate);
             }
         };
 
         vm.getSearchEngineContribution = function () {
             $http.get(baseUrl + 'sessions/overview/sources/se').success(function (data) {
+                vm.searchEngines = data;
+            });
+        };
+
+        vm.getSearchEngineContributionByRange = function (startDate,endDate) {
+            $http.get(baseUrl + 'sessions/overview/sources/se/' + startDate + '/' + endDate + '/10').success(function (data) {
                 vm.searchEngines = data;
             });
         };
@@ -65,8 +87,20 @@ angular.module('vislog.overview', ['chart.js'])
             });
         };
 
+        vm.getCountriesContributionByRange = function (startDate,endDate) {
+            $http.get(baseUrl + 'sessions/overview/sources/countries/' + startDate + '/' + endDate + '/10').success(function (data){
+                vm.countryContribution = data;
+            });
+        };
+
         vm.getHotPages = function() {
             $http.get(baseUrl + 'sessions/overview/frequent/pages').success(function (data) {
+                vm.hotPages = data;
+            });
+        };
+
+        vm.getHotPagesByRange = function(startDate,endDate) {
+            $http.get(baseUrl + 'sessions/overview/frequent/pages/'  + startDate + '/' + endDate + '/20').success(function (data) {
                 vm.hotPages = data;
             });
         };
@@ -75,6 +109,21 @@ angular.module('vislog.overview', ['chart.js'])
             $http.get(baseUrl + 'sessions/overview/frequent/categories').success(function (data) {
                 var dups = [];
                 var i,n;
+
+                for (i=0, n=data.length; i <n; i++) {
+                    vm.hotCategories.labels.push(data[i].name);
+                    dups.push(data[i].dup);
+                }
+                vm.hotCategories.data.push(dups);
+            });
+        };
+
+        vm.getHotCategoriesByRange = function(startDate,endDate) {
+            $http.get(baseUrl + 'sessions/overview/frequent/categories/'  + startDate + '/' + endDate + '/7').success(function (data) {
+                var dups = [];
+                var i,n;
+                vm.hotCategories.labels = [];
+                vm.hotCategories.data = [];
 
                 for (i=0, n=data.length; i <n; i++) {
                     vm.hotCategories.labels.push(data[i].name);
@@ -96,6 +145,21 @@ angular.module('vislog.overview', ['chart.js'])
             });
         };
 
+        vm.getMainLandingCategoriesByRange = function(startDate,endDate) {
+            $http.get(baseUrl + 'sessions/overview/landings/categories/'  + startDate + '/' + endDate + '/10').success(function (data) {
+                var dups = [];
+                var i, n;
+                vm.mainLandingCategories.labels = [];
+                vm.mainLandingCategories.data = [];
+
+                for (i=0, n=data.length; i<n; i++) {
+                    vm.mainLandingCategories.labels.push(data[i].name);
+                    dups.push(data[i].dup);
+                }
+                vm.mainLandingCategories.data.push(dups);
+            });
+        };
+
         vm.getMainDropOffCategories = function () {
             $http.get(baseUrl + 'sessions/overview/dropoff/categories').success(function (data) {
                 var dups = [];
@@ -108,13 +172,28 @@ angular.module('vislog.overview', ['chart.js'])
             });
         };
 
-        vm.getOverviewNumbers();
-        vm.getSessionDistributionByDate("2014-10-22");
-        vm.getSearchEngineContribution();
-        vm.getCountriesContribution();
-        vm.getHotPages();
-        vm.getHotCategories();
-        vm.getMainLandingCategories();
-        vm.getMainDropOffCategories();
+        vm.getMainDropOffCategoriesByRange = function (startDate,endDate) {
+            $http.get(baseUrl + 'sessions/overview/dropoff/categories/'  + startDate + '/' + endDate + '/10').success(function (data) {
+                var dups = [];
+                var i, n;
+                vm.mainDropOffCategories.labels = [];
+                vm.mainDropOffCategories.data = [];
+
+                for (i=0, n=data.length; i<n; i++) {
+                    vm.mainDropOffCategories.labels.push(data[i].name);
+                    dups.push(data[i].dup);
+                }
+                vm.mainDropOffCategories.data.push(dups);
+            });
+        };
+
+        vm.getOverviewNumbersByRange("2014-08-08","2014-08-11");
+        vm.getSessionDistributionByRange("2014-08-08","2014-08-11");
+        vm.getSearchEngineContributionByRange("2014-08-08","2014-08-11");
+        vm.getCountriesContributionByRange("2014-08-08","2014-08-11");
+        vm.getHotPagesByRange("2014-08-08","2014-08-11");
+        vm.getHotCategoriesByRange("2014-08-08","2014-08-11");
+        vm.getMainLandingCategoriesByRange("2014-08-08","2014-08-11");
+        vm.getMainDropOffCategoriesByRange("2014-08-08","2014-08-11");
 
     });
